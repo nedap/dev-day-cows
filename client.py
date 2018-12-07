57ff211946829b024d332d3d0a380993e8dff5f5
Erik Huizinga <erik.huizinga@nedap.com>
2018-12-07 15:43:26 +0100

    🎨 Clean up client

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ import json
 import collections
 from solution import getSensorId, calculatePosition
 
-SENSOR_IP = 'p.q.r.s'
+SENSOR_IP = 'p.q.r.s'  # Config this later
 SENSOR_PORT = 9000
 BUFFER_SIZE = 1024
 
@@ -21,37 +21,41 @@ BEACON2 = Beacon(id=11, x=1500, y=2000, z=0)
 BEACON3 = Beacon(id=7, x=1400, y=800, z=0)
 BEACON4 = Beacon(id=9, x=2400, y=1200, z=0)
 
-BeaconWithRssi = collections.namedtuple('BeaconWithRssi', ['beacon', 'rssi'])
+BeaconRSSI = collections.namedtuple('BeaconWithRssi', ['beacon', 'rssi'])
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((SENSOR_IP, SENSOR_PORT))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect((SENSOR_IP, SENSOR_PORT))
 
-m = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-m.connect((MAP_IP, MAP_PORT))
+mapper = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mapper.connect((MAP_IP, MAP_PORT))
 
-buffer = ""
+buf = ""
 while True:
-    # print "Wait for sensor data...."
-    data = s.recv(BUFFER_SIZE)
-    # print "Sensor data received"
-    buffer += data
-    index = buffer.index(START)
-    if (index == 0):
+    # Wait for sensor data...
+    data = server.recv(BUFFER_SIZE)
+
+    # Sensor data received!
+    buf += data
+
+    index = buf.index(START)
+    if index == 0:
         index += len(START)
-        buffer = buffer[index:]
+        buf = buf[index:]
     else:
         continue
 
-    lines = buffer.split("\n")
+    lines = buf.split("\n")
+
     for line in lines:
-        buffer = ""
-        if (line.endswith(END)):
+        buf = ""
+
+        if line.endswith(END):
             line = line.replace(START, "")
             line = line.replace(END, "")
             sensor = json.loads(line)
             sensorId = sensor["sensorId"]
             beacons = sensor["beacons"]
-            # print line
+
             if sensorId == getSensorId():
                 beaconList = []
                 for beacon in beacons:
@@ -59,22 +63,25 @@ while True:
                 print sensorId + ": ", beaconList
                 # print line
 
-                b1 = BeaconWithRssi(beacon=BEACON1, rssi=beaconList[BEACON1.id])
-                b2 = BeaconWithRssi(beacon=BEACON2, rssi=beaconList[BEACON2.id])
-                b3 = BeaconWithRssi(beacon=BEACON3, rssi=beaconList[BEACON3.id])
-                b4 = BeaconWithRssi(beacon=BEACON4, rssi=beaconList[BEACON4.id])
+                b1 = BeaconRSSI(beacon=BEACON1, rssi=beaconList[BEACON1.id])
+                b2 = BeaconRSSI(beacon=BEACON2, rssi=beaconList[BEACON2.id])
+                b3 = BeaconRSSI(beacon=BEACON3, rssi=beaconList[BEACON3.id])
+                b4 = BeaconRSSI(beacon=BEACON4, rssi=beaconList[BEACON4.id])
+
                 x, y, z = calculatePosition(b1, b2, b3, b4)
 
                 x = int(x)
                 y = int(y)
 
-                print "Sent position to map: " + `x` + "," + `y`
-                m.send(sensorId + "," + `x` + "," + `y` + "\n")
-                print "Position sent to map"
-                print "Wait for okay from map"
-                data = m.recv(BUFFER_SIZE)
+                print "Sending position to mapper: " + str(x) + "," + str(y)
+                mapper.send(sensorId + "," + str(x) + "," + str(x) + "\n")
+                print "Position sent to mapper"
+
+                print "Wait for okay from mapper"
+                data = mapper.recv(BUFFER_SIZE)
                 print "Received result from map: ", data
         else:
-            buffer = line;
-s.close()
-m.close();
+            buf = line
+
+server.close()
+mapper.close()
